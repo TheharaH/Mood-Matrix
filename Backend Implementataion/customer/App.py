7cae0e8d4325feee21139cf69f83a105b1674916
TheharaH <thehara00@gmail.com>
2025-03-10 23:36:55 +0530

    updated flask code

--- Backend Implementataion/customer/App.py
+++ Backend Implementataion/customer/App.py
@@ -1,37 +1,3 @@
-'''from flask import Flask, request, jsonify
-from flask_cors import CORS
-import joblib
-
-app = Flask(__name__)
-CORS(app)  # Allow cross-origin requests (React will call this API)
-
-# Load trained model and vectorizer
-model = joblib.load("sentiment_model.pkl")
-vectorizer = joblib.load("tfidf_vectorizer.pkl")
-
-
-@app.route("/predict", methods=["POST"])
-def predict():
-    try:
-        data = request.json
-        review = data.get("review", "")
-        if not review:
-            return jsonify({"error": "Empty review"}), 400
-
-        transformed_review = vectorizer.transform([review])  # Convert to numerical form
-        prediction = model.predict(transformed_review)[0]  # Predict sentiment
-
-        result = "Positive" if prediction == 1 else "Negative"
-        return jsonify({"review": review, "sentiment": result})
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-'''
-
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
